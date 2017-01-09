ca829f20a147d8749181e26f55a95cd997841e93
Mark Hahn <mark@hahnca.com>
2017-01-08 19:36:08 -0800

    aurelia xy app

---
+++ src/ssid_list.js
@@ -0,0 +1,24 @@
+
+import {HttpClient} from 'aurelia-http-client';
+
+
+export class ssid_list {
+  constructor() {
+    this.heading = "SSIDs";
+    this.ssids = [];
+
+    let client = new HttpClient();
+    client.get('ssids').then(data => { console.log(data[0].name) });
+  }
+
+  add(name) {
+    this.ssids.push(name);
+  }
+
+  // add(name) {
+  //   let index = this.ssids.indexOf(name);
+  //   if (index !== -1) {
+  //     this.todos.splice(index, 1);
+  //   }
+  // }
+}
